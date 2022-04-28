8b2d05bc663845d6bd6f0910ebfa5edf68adedea
SanderRuusmaa <sander.ruusmaa@gmail.com>
2022-04-28 19:13:09 +0300

    simple navbar added

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { useNavigate } from "react-router-dom";
+
+import './Navbar.scss'
+
+import RammukoopaLogo from '../../imgs/Rammukoobas_vector_png.png'
+
+function Navbar() {
+
+	const navigate = useNavigate()
+
+	const routeChange = (path) => {
+		navigate(path)
+	}
+
+	return (
+		<header className='heading display-flex'>
+			<div className='width-40 display-flex'>
+				<h2 onClick={() => routeChange("/voistlused")}>VÕISTLUSED</h2>
+				<h2 onClick={() => routeChange("/kontakt")}>KONTAKT</h2>
+			</div>
+
+			<div className='logo-container'>
+				<img id='navbar-logo' src={RammukoopaLogo} alt="Logo" />
+			</div>
+
+			<div className='width-40 display-flex'>
+				<h2 onClick={() => routeChange("/klubist")}>KLUBIST</h2>
+				<h2 onClick={() => routeChange("/liikmed")}>LIIKMED</h2>
+			</div>
+		</header>
+	)
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Homepage from './Homepage/Homepage';
+import Homepage from './Components/Homepage/Homepage';
 import reportWebVitals from './reportWebVitals';
 
 import 'semantic-ui-css/semantic.min.css'
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Homepage />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+
   </React.StrictMode>
 );
 
