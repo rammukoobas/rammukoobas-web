722ced5433a6e7b89f385cc3eaec6b3d87401a13
Sander Ruusmaa <Sander.Ruusmaa@datel.ee>
2022-05-03 20:50:08 +0300

    sponsors added, about text changed

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,6 +4,8 @@ import './About.scss'
 
 import SkacronLogo from '../../imgs/skarcon.png'
 import KiilakaLogo from '../../imgs/painted-by-kiilakas-logo.png'
+import KoverKang from '../../imgs/koverkang.png'
+import Santrust from '../../imgs/santrust.png'
 
 function About() {
 	return (
@@ -17,7 +19,7 @@ function About() {
 			<div className='joon veelvaiksem'></div>
 
 			<div className='tekst'>
-				<h3>Rammuklubi MTÜ loodi 2021 aastal eesmärgiga tuua kokku jõutõstmise ja rammualade inimesed Tartus ja lähiümbruses ning luua klubi liikmetele spetsiaalne treeningruum, mis on täna tuntud kui Rammukoobas. Aktiivsemad klubi liikmed korraldavad ka võistlusi ja treeningpäevi Rammukoopas, nendest üritustest leiab infot meie Facebook ja Instagrami leheküljelt. Läbi oma tegevuse tahame anda panuse rammuspordi kasvu ja levikusse Eestis. </h3>
+				<h3>Rammuklubi MTÜ loodi 2021 aastal eesmärgiga tuua kokku jõutõstmise ja rammualade inimesed Tartus ja lähiümbruses ning luua klubi liikmetele spetsiaalne treeningruum, mis on täna tuntud kui Rammukoobas. Aktiivsemad klubi liikmed korraldavad võistlusi ja treeningpäevi Rammukoopas, nendest üritustest leiab infot meie Facebook ja Instagrami leheküljelt. Samuti käivad klubi liikmed ise võistlemas nii Eestis kui välisriikides. Läbi oma tegevuse tahame anda panuse jõu- ja rammuspordi kasvu ning levikusse Eestis. </h3>
 				<h3>Klubi toimib tänu liikmete toetustele ja annetustele. Rammukoopa sisustuse ja inventari on klubi liikmed ise hankinud või on saadud see läbi toetajate. Selle eest ütleme koopa käimashoidmise eest nii oma liikmetele kui toetajatele: "AITÄH!" </h3>
 				<h3 className='center'>Alati võib meid toetada tehes annetuse meie pangakontole:</h3>
 				<h2 className='center'>Rammuklubi MTÜ</h2>
@@ -34,6 +36,14 @@ function About() {
 					<div className='logo'>
 						<img src={KiilakaLogo} alt="kiilaka logo" />
 						<a href='http://kiilakas.ee/' target="_blank" rel="noopener noreferrer"><h3>Kiilaka Autod OÜ</h3></a>
+					</div>
+					<div className='logo'>
+						<img src={Santrust} alt="santrust logo" />
+						<a href='https://www.facebook.com/SanTrustoy' target="_blank" rel="noopener noreferrer"><h3>Santrust OÜ</h3></a>
+					</div>
+					<div className='logo'>
+						<img src={KoverKang} alt="KoverKang logo" />
+						<a href='https://www.instagram.com/koverkang/' target="_blank" rel="noopener noreferrer"><h3>Kõver Kang</h3></a>
 					</div>
 
 				</div>
