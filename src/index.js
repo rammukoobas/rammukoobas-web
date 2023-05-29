c7fcc8f9c46b530ac97040042084662dc8cf4093
Sander Ruusmaa <sander.ruusmaa@gmail.com>
2023-05-29 09:28:53 +0300

    Removed popup alert

--- src/index.js
+++ src/index.js
@@ -17,26 +17,26 @@ import { PopupAlert } from './Components/PopupAlert/PopupAlert';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/klubist" element={<About />} />
-        <Route path="/kontakt" element={<Contact />} />
-        <Route path="/liikmed" element={<Members />} />
-        <Route path="/voistlused/:voistlus" element={<Competition />} />
-        <Route path="/voistlused/:voistlus/live" element={<Competition infoDisplayed="live" />} />
-		<Route path="/voistlused/:voistlus/video" element={<Competition infoDisplayed="live" />} />
-        <Route path="/voistlused/:voistlus/register" element={<Competition infoDisplayed="register" />} />
-        <Route path="/voistlused" element={<Competitions />} />
-        <Route path="*" element={<Homepage />} />
-      </Routes>
-	  <PopupAlert />
-      <Footer />
-    </HashRouter>
+	<React.StrictMode>
+		<HashRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="/klubist" element={<About />} />
+				<Route path="/kontakt" element={<Contact />} />
+				<Route path="/liikmed" element={<Members />} />
+				<Route path="/voistlused/:voistlus" element={<Competition />} />
+				<Route path="/voistlused/:voistlus/live" element={<Competition infoDisplayed="live" />} />
+				<Route path="/voistlused/:voistlus/video" element={<Competition infoDisplayed="live" />} />
+				<Route path="/voistlused/:voistlus/register" element={<Competition infoDisplayed="register" />} />
+				<Route path="/voistlused" element={<Competitions />} />
+				<Route path="*" element={<Homepage />} />
+			</Routes>
+			{/* Popup alert goes here if necessary */}
+			<Footer />
+		</HashRouter>
 
-  </React.StrictMode>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
